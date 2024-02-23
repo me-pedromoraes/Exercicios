8eed586b5f2a037e61a9153383a73623302e2338
me-pedromoraes <pedrohmsjc@gmail.com>
2024-02-23 19:52:22 -0300

    exercicio1

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("olá");
+// EXERCICIO #1
+    // Criar um programa o qual leia a idade do usuário e diga se 
+    // ele já pode tirar habilitação (acima de 18 anos).
+
+let idade = 18
+
+if (idade >= 18 ){
+    console.log("Pode tirar habilitação");
+    
+}
+else {
+    console.log("Não pode tirar habilitação");
+
+}
