07d2a93881eeb9c58406a0ec55c6c0e4cece5602
me-pedromoraes <pedrohmsjc@gmail.com>
2024-02-23 20:33:37 -0300

    exercicio 3

---
+++ exercicio3.js
@@ -0,0 +1,12 @@
+// Exercício #3
+    // Faça um programa que leia um número de entrada e retorne se é PAR ou ÍMPAR:
+
+    let numero = 49
+
+    function verifParOuImpar(numero){
+        if (numero % 2 === 0){
+        console.log("Número PAR");
+}
+        else { console.log("Número ÍMPAR");
+        }
+}
