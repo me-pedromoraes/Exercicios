465639c394ccc498aa395acae47b612b3e967990
me-pedromoraes <pedrohmsjc@gmail.com>
2024-02-23 20:56:28 -0300

    exercicio4

---
+++ exercicio4.js
@@ -0,0 +1,10 @@
+// Exercício #4
+    // Faça um programa que calcule a área de um trapézio. Fórmula: A=(a+b)h/2
+
+// Inserir as informações abaixo:
+let a= 20
+let b= 30
+let h= 45
+let A= "Área do trapézio "
+
+console.log(A + (a + b)*h/2);
