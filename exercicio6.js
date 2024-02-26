4e6596cdeccb381eedf9fded08064c20256103b3
me-pedromoraes <pedrohmsjc@gmail.com>
2024-02-26 20:01:51 -0300

    exercicio 6

---
+++ exercicio6.js
@@ -0,0 +1,15 @@
+// EXERCÍCIO #6
+    // Crie um programa que retorne se dois números são iguais, e qual o maior
+
+let numeroA = 90
+let numeroB = 90
+
+if (numeroA === numeroB){
+    console.log("os números são iguais");
+}
+
+else if (numeroA > numeroB){
+    console.log(numeroA + " é maior que " + numeroB);
+}
+
+else console.log(numeroB + " é maior que " + numeroA);
