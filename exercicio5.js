49a8622dc9b91167bcf1789597582895630faab5
me-pedromoraes <pedrohmsjc@gmail.com>
2024-02-26 19:41:24 -0300

    exercicio 5

---
+++ exercicio5.js
@@ -0,0 +1,8 @@
+// EXERCÍCIO 5
+    // Crie um programa que calcule a média entre dois números
+
+//Insira os números abaixo:
+let numeroA=56
+let numeroB=78
+
+console.log("a média é: " + (numeroA + numeroB)/ 2);
